60e31d0979ecc1a307bb74a79bd92de60341397d
Vincent Rizzo <vincent@datamaran.com>
2022-12-04 22:37:54 +0100

    Clase 84

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  constructor(private gifsService: GifsService) {}
+  get history() {
+    return [...this.gifsService.history];
+  }
+}
