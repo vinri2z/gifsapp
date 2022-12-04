e2be1770c9a31f8ac9593953e6d95846dbf805c1
Vincent Rizzo <vincent@datamaran.com>
2022-12-04 22:24:18 +0100

    Clase 83

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule],
+  imports: [BrowserModule, SharedModule, GifsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
